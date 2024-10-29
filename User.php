fb90b122ab0546e84581ad4d8a16af88c507113f
Magnus Karström <mrkarstrom81@gmail.com>
2024-10-29 19:58:59 +0100

    Init

---
+++ User.php
@@ -0,0 +1,13 @@
+<?php
+
+class User
+{
+    public $first_name;
+
+    public $last_name;
+
+    public function getFullName()
+    {
+        return trim("$this->first_name $this->last_name");
+    }
+}
---
+++ tests/UserTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    public function testUserReturnsFullName()
+    {
+        require 'User.php';
+
+        $user = new User;
+
+        $user->first_name = 'Magnus';
+        $user->last_name = 'Karström';
+
+        $this->assertEquals('Magnus Karström', $user->getFullName());
+    }
+
+    public function testFullNameIsEmptyByDefault()
+    {
+        $user = new User;
+
+        $this->assertEquals('', $user->getFullName());
+    }
+
+    public function testUserHasFirstName()
+    {
+        $user = new User;
+
+        $user->first_name = 'Magnus';
+
+        $this->assertEquals('Magnus', $user->first_name);
+    }
+}
